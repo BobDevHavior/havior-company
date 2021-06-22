e1885c7d89edf1f45ab5f01ff7b90cca2013f747
jonathan serafini <jonathan.serafini@gmail.com>
2021-06-22 10:42:10 +0200

    Initial commit with devise template from Mihivai

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,20 @@
+require("@rails/ujs").start()
+require("@rails/activestorage").start()
+require("channels")
+
+var Turbolinks = require("turbolinks")
+Turbolinks.start()
+
+// External imports
+import "bootstrap";
+
+// Internal imports
+import { initCookieBanner } from '../components/cookies'
+
+document.addEventListener('turbolinks:load', () => {
+  initCookieBanner();
+  // Call your functions here, e.g:
+  // initSelect2();
+});
+
+import "controllers"
